c295d4de10f74d4436cf304b48c49190b1f71fde
Michael Voronov <michael.voronov@gmail.com>
2021-03-09 21:41:25 +0300

    minor fix

--- src/app/weather-forecast/weather-forecast.component.ts
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -2,19 +2,7 @@ import {Component} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-
-export interface Card {
-  country: string;
-  city: string;
-  temperature: string;
-  pressure: number;
-  wind: string;
-  precipitation: string;
-}
-
-export interface Selector {
-  name: string;
-}
+import { WeatherService } from '../wether.service';
 
 @Component({
   selector: 'app-weather-forecast',
@@ -23,78 +11,22 @@ export interface Selector {
 })
 
 export class WeatherForecastComponent {
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private service: WeatherService
+  ){}
 
-  countries: Selector[] = [{
-    name: 'Russia'
-  },
-    {
-      name: 'Norway'
-    },
-    {
-      name: 'USA'
-    }, ];
-  cards: Card[] = [{
-    country: 'Russia',
-    city: 'Saint-Petersburg',
-    temperature: '-5',
-    pressure: 1032,
-    wind: '32%',
-    precipitation: '7 m/s',
-  },
-    {
-      country: 'Russia',
-      city: 'Moscow',
-      temperature: '-2',
-      pressure: 1032,
-      wind: '35%',
-      precipitation: '8 m/s',
-    },
-    {
-      country: 'Russia',
-      city: 'Yekaterinburg',
-      temperature: '-10',
-      pressure: 1032,
-      wind: '35%',
-      precipitation: '8 m/s',
-    },
-    {
-      country: 'Russia',
-      city: 'Kazan',
-      temperature: '-7',
-      pressure: 1032,
-      wind: '35%',
-      precipitation: '8 m/s',
-    },
-  {
-    country: 'Norway',
-    city: 'Oslo',
-    temperature: '-1',
-    pressure: 1032,
-    wind: '32%',
-    precipitation: '7 m/s',
-  },
-  {
-    country: 'USA',
-    city: 'Miami',
-    temperature: '+20',
-    pressure: 1032,
-    wind: '15%',
-    precipitation: '7 m/s',
-  }];
+  selectedCountriesControl = new FormControl(this.service.getCountries()[0]);
 
-  selectedCountriesControl = new FormControl(this.countries[0]);
-
-  selectedCards = this.cards.filter(item => item.country === this.countries[0].name);
+  selectedCards = this.service.cards.filter(item => item.country === this.service.getCountries()[0].name);
 
   handleChange(event: any): any {
-    const selectedCards: any = this.cards.filter(item => item.country === event.value.name);
-    this.selectedCards = selectedCards;
-    // this.country = event.value.name;
-    // console.log(this.country);
-    if (event.value.name) {
-      this.router.navigate([event.value.name]);
-    }
-    return false;
+    this.router.navigate([event.value.name]);
+    // // const selectedCards: any = this.cards.filter(item => item.country === event.value.name);
+    // // this.selectedCards = selectedCards;
+    // if (event.value.name) {
+    //   this.router.navigate([event.value.name]);
+    // }
+    // return false;
   }
 }
